8e4ed37cb2ec1d56d7dc9c01850da6c619449cc9
Jessica-Franca <jealunaifsp@gmail.com>
2023-03-10 16:44:48 -0300

    database connection setup

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :sales_management_system, SalesManagementSystem.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "admin",
   database: "sales_management_system_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
